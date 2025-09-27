2b715bb9480755c3c06162f4caa55b555d9b771e
Harshad0804 <harshadkarguwal@gmail.com>
2025-09-27 16:27:49 +0530

    Python

---
+++ List.py
@@ -0,0 +1,13 @@
+List=["Harshad",1,2,3,"Harsh",2.5,5.4,True,3.5,False,"Jigar"]
+
+List.append(5)
+print(List)
+
+List2=List.copy()
+print(List2)
+
+List2.pop()
+print(List2)
+
+List.extend([3,5])
+print(List)
