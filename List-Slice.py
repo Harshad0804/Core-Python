f6e46de98e3c198f61d85d8e1618c42d9734a449
Harshad0804 <harshadkarguwal@gmail.com>
2025-09-29 13:35:07 +0530

    Python

---
+++ List-Slice.py
@@ -0,0 +1,17 @@
+list=[1,2,3,56,"Harshad","Java",True,89.78,"Karguwal",False,10,20,50,"Monday","Tops","Python","Harsh",100,500]
+
+print(list[:2:5])
+print(list[15::5])
+print(list[1:3:3])
+print(list[::16])
+print(list[10:16])
+
+print(list[:-17:17])
+print(list[-15::10])
+print(list[-16:-14:5])
+print(list[::-9])
+print(list[-13:-7])
+
+
+
+
