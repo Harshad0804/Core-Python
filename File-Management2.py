5290bc48a9ba70afef92b30680e1ce59dbe1250e
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-01 15:03:27 +0530

    Python

--- File-Management2.py
+++ File-Management2.py
@@ -5,3 +5,16 @@ for i in range(10):
     num=random.randint(1,100)
     data.write(str(num)+",")
 data.close()
+
+data=open("data.txt","r")
+even=open("even.txt","w")
+odd=open("odd.txt","w")
+l=data.read().split(",")[:-1]
+for i in l:
+    if int(i)%2==0:
+        even.write(i+",")
+    else:
+        odd.write(i+",")
+data.close()
+even.close()
+odd.close()
