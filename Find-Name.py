614871bc0cec01443ae08f47c1a72e2527e87b6f
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-23 16:56:09 +0530

    Python

---
+++ Find-Name.py
@@ -0,0 +1,8 @@
+names = ("Harshad", "Dhruv", "Vicky", "Lalit")
+search_name = input("Enter the name to search: ")
+
+if search_name in names:
+    print(f"{search_name} found in tuple")
+else:
+    print(f"{search_name} not found in tuple")
+n
