4cafd8a005c42883707a267e51448c16babc2b26
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-30 19:55:11 +0530

    Practice

---
+++ Arithematic-operations.py
@@ -0,0 +1,10 @@
+a = 7
+b = 2
+
+print('Sum:', a + b)           # 9
+print('Subtraction:', a - b)   # 5
+print('Multiplication:', a * b)# 14
+print('Division:', a / b)      # 3.5
+print('Floor Division:', a // b)# 3
+print('Modulo:', a % b)        # 1
+print('Power:', a ** b)        # 49
